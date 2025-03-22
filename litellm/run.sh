f01eea542026e79fb5b5e2a9a0a9503194f86ea0
Diego Alvarez <info@diegoa.ca>
2025-03-21 17:54:38 -0700

    so close, use right method
    Fixes:
    
    ```
    s6-rc: info: service legacy-services successfully started
    [17:36:16] INFO: Writing configuration to /app/config.yaml
    /run.sh: line 12: bashio::config.get: command not found
    ```

--- litellm/run.sh
+++ litellm/run.sh
@@ -9,7 +9,7 @@ mkdir -p /app
 
 # Get the config from options and write it to the config file
 bashio::log.info "Writing configuration to ${CONFIG_PATH}"
-bashio::config.get "config" > ${CONFIG_PATH}
+bashio::config "config" > ${CONFIG_PATH}
 
 # Start LiteLLM with the config file and configured port
 bashio::log.info "Starting LiteLLM proxy server on port ${PORT}"
