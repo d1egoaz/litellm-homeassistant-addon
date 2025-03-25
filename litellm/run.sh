7d3a17876465aea426598780408e30910351e7f7
Diego Alvarez <info@diegoa.ca>
2025-03-24 21:24:52 -0700

    Update run.sh to remove nonexisting option

--- litellm/run.sh
+++ litellm/run.sh
@@ -20,19 +20,4 @@ bashio::log.info "Starting LiteLLM proxy server on port ${PORT}"
 
 # Try running with verbose output
 bashio::log.info "Running LiteLLM with verbose output"
-exec litellm --config ${CONFIG_PATH} --port ${PORT} --verbose || {
-    EXIT_CODE=$?
-    bashio::log.error "LiteLLM failed to start. Error code: ${EXIT_CODE}"
-
-    # Try to get more info about the failure
-    bashio::log.info "Trying to run litellm with --help to verify command exists:"
-    litellm --help || bashio::log.error "litellm --help failed, command may be invalid"
-
-    # List directory contents to check permissions
-    bashio::log.info "Directory contents of /app:"
-    ls -la /app
-
-    # Wait before exiting to capture logs
-    sleep 30
-    exit ${EXIT_CODE}
-}
+exec litellm --config ${CONFIG_PATH} --port ${PORT}
